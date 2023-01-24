3bb5a02a0d9bb3194130b50fcdc66f5606510a2a
Jeff Scott <jpscott@ranken.edu>
2023-01-24 11:21:38 -0600

    Responsive Website Design Created T 1/24/2023

---
+++ js/size.js
@@ -0,0 +1,7 @@
+//	https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onresize
+function myFunction() {
+  let w = window.outerWidth;
+  let h = window.outerHeight;
+  let txt = "Window size: width = " + w + " and height = " + h;
+  alert(txt);
+}
